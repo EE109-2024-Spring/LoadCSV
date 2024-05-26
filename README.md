0d88e1c291599334edab32ab72d9a28d1c57e730
Gina Sohn <gina7484@gmail.com>
2024-05-26 00:37:11 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Loading data from csv files in Spatial
+This repository contains code to explain how to use csv files to load data into [Spatial](https://github.com/stanford-ppl/spatial).
+
---
+++ src/test/scala/Lab1.scala
@@ -0,0 +1,20 @@
+import spatial.dsl._
+
+
+@spatial class loadcsv2d extends SpatialTest {
+    
+  type T = FixPt[TRUE,_16,_16]
+
+  def main(args: Array[String]): Unit = {
+    
+    // These are on the HOST
+    val A_host = loadCSV2D[T](s"$DATA/mat.csv")
+    val A_dram = DRAM[T](3,2)
+    setMem(A_dram, A_host)
+    
+    val a_mat = getMatrix(A_dram)
+    printMatrix(a_mat)
+    writeCSV2D(a_mat, s"$DATA/output.csv")
+    assert(Bit(true))
+  }
+}
